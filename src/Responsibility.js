eae9fb6c643b27b2aae4cb91ae78d655fc5b2fed
gcamilo <gcamilo7@gmail.com>
2020-04-08 01:38:11 -0300

    test w div

--- src/Responsibility.js
+++ src/Responsibility.js
@@ -53,12 +53,17 @@ class Responsibility extends React.Component {
 
     render() {
         const entity = this.state.entities[this.state.currEntity];
+        console.log(this.state.posNeg);
         return (
             <div>
                 <div>
                     {this.state.entityQuestions.map((question, qIdx) => (
+                        (question.id === "Punish" && (this.state.posNeg === "pos" || this.state.posNeg === "con"))?
+                        <div></div>
+                        :
                         // !(question.id === "Punish" && this.state.posNeg === "pos")? 
-                        <div style={(question.id === "Punish" && (this.state.posNeg === "pos" || this.state.posNeg === "con"))? {display: "none"}: {display: "initial"}} key={qIdx} className="Question">
+                        //<div style={(question.id === "Punish" && (this.state.posNeg === "pos" || this.state.posNeg === "con"))? {display: "none"}: {}} key={qIdx} className="Question">
+                        <div key={qIdx} className="Question">
                             <span className="Question" style={{margin: "0px"}}>{question.initial[[this.state.posNeg]]} </span>
                             <span className="Spotlight Question" style={{margin: "0px"}}> {entity.text} </span> 
                             <span className="Question" style={{margin: "0px"}}> {question.conclusion[[this.state.posNeg]]}</span>
