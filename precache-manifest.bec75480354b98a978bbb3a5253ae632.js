5b9ca65b266f0a68280dfe817f33bf255810a6dd
gcamilo <gcamilo7@gmail.com>
2020-05-02 14:26:19 -0300

    Updates

---
+++ precache-manifest.bec75480354b98a978bbb3a5253ae632.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd42c866171356d9d83c55711f39e2b6",
+    "url": "/ai-vaccine-pollfish/index.html"
+  },
+  {
+    "revision": "7a5799a4fe7b66288ac3",
+    "url": "/ai-vaccine-pollfish/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "e1b01ca682f265bdd66a",
+    "url": "/ai-vaccine-pollfish/static/css/main.7284f95e.chunk.css"
+  },
+  {
+    "revision": "7a5799a4fe7b66288ac3",
+    "url": "/ai-vaccine-pollfish/static/js/2.a002297e.chunk.js"
+  },
+  {
+    "revision": "3ee64a3963b797a3e3ba1309b348eddb",
+    "url": "/ai-vaccine-pollfish/static/js/2.a002297e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b01ca682f265bdd66a",
+    "url": "/ai-vaccine-pollfish/static/js/main.4d8c2ea6.chunk.js"
+  },
+  {
+    "revision": "6878b858a53844dadad7",
+    "url": "/ai-vaccine-pollfish/static/js/runtime-main.00c8983e.js"
+  },
+  {
+    "revision": "009932ce249157d6860f645f5290f8f5",
+    "url": "/ai-vaccine-pollfish/static/media/AI.009932ce.png"
+  },
+  {
+    "revision": "b738c7a7202b72b464e65f3d43c17498",
+    "url": "/ai-vaccine-pollfish/static/media/con_.b738c7a7.png"
+  },
+  {
+    "revision": "224d0a7754146f675c7e2bd72c6381c7",
+    "url": "/ai-vaccine-pollfish/static/media/human-AI.224d0a77.png"
+  },
+  {
+    "revision": "170a0e836b7a1bf5116a438ded010863",
+    "url": "/ai-vaccine-pollfish/static/media/human.170a0e83.png"
+  },
+  {
+    "revision": "2a9d07ee44176836a7a0d436826307e9",
+    "url": "/ai-vaccine-pollfish/static/media/neg_.2a9d07ee.png"
+  },
+  {
+    "revision": "62f648b1c8b175a17e44bf97af10980d",
+    "url": "/ai-vaccine-pollfish/static/media/pos_.62f648b1.png"
+  }
+]);
